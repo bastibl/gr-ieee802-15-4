2a73faaf5ac90aca451d2a60e697fd4fa1bf252a
Felix Wunsch <uncnr@student.kit.edu>
2014-10-15 12:45:41 +0200

    removed the matlab files. branch name is now somewhat misleading...

--- matlab/css_bit_interleaver.m
+++
@@ -1,4 +0,0 @@
-function [out] = css_bit_interleaver(in)
-    intlv_seq = [1:4 53:56 9:12 61:64 17:20 37:40 25:28 45:48 33:36 21:24 41:44 61:64 49:52 5:8 57:60 13:16];
-    out = in(intlv_seq);
-end
