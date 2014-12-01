559fa74899eee9af8ee8091eae7ab0fd5f285bd4
Felix Wunsch <uncnr@student.kit.edu>
2014-12-01 16:38:21 +0100

    added EbN0 plot

--- examples/rayleigh_multipath/combine_rayleigh_ber_curves.py
+++ examples/rayleigh_multipath/combine_rayleigh_ber_curves.py
@@ -21,6 +21,8 @@ if __name__ == "__main__":
             pdp[i] = 0
     print pdp
 
+
+    # SNR plots
     f, axarr = plt.subplots(2)
 
     axarr[1].plot(snr_oqpsk, oqpsk, label="OQPSK")
@@ -30,6 +32,39 @@ if __name__ == "__main__":
     axarr[1].grid()
     axarr[1].set_yscale('log')
     axarr[1].set_xlabel("SNR")
+    # axarr[1].set_xlim([-25,-3])
+    axarr[1].set_ylabel("BER")
+    axarr[1].set_title("Bit error rates in Rayleigh channel with AWGN")
+
+    markerline, stemlines, baseline = axarr[0].stem(t*1e9, pdp, '-', bottom=0.00001)
+    axarr[0].set_xlabel("delay in ns")
+    axarr[0].set_xlim([-10, max(t)*1e9])
+    axarr[0].set_ylabel("normalized power")
+    axarr[0].grid()
+    axarr[0].set_ylim([0.0001,8])
+    axarr[0].set_title("Power delay profile")
+    axarr[0].set_yscale('log')
+    plt.savefig("ber_rayleigh_combined_"+time.strftime("%Y-%m-%d_%H-%M-%S")+".pdf")
+    plt.show()
+
+    # EbN0 plots
+    f, axarr = plt.subplots(2)
+    css_Eb_noSF = 19.0007 # sum(|norm_fac*chirp_seq|^2)/8 --> Energy per code bit
+    css_Eb_slow = css_Eb_noSF*32.0/6
+    css_Eb_fast = css_Eb_noSF*4.0/3
+    # this Eb/N0 does not consider energy spent on headers, only the payload including the respective coding rate is considered because otherwise the number of payload bytes per frame would gain influence
+    css_EbN0_fast = 10*np.log10(css_Eb_loSF) + snr_css_fast
+    css_EbN0_slow = 10*np.log10(css_Eb_hiSF) + snr_css_slow
+    # OQPSK
+    oqpsk_Eb = 64.0/4 # the OQPSK signal has always magnitude 1; one codeword is 64 samples long and encodes 4 payload bits (in the 2450 MHz band)
+    oqpsk_EbN0 = 10*np.log10(oqpsk_Eb) + snr_oqpsk
+    axarr[1].plot(EbN0_oqpsk, oqpsk, label="OQPSK")
+    axarr[1].plot(EbN0_css_fast, css_fast, label="CSS 1 Mbps (SD)")
+    axarr[1].plot(EbN0_css_slow, css_slow, label="CSS 250 kbps (SD)")
+    axarr[1].legend(loc='lower left')
+    axarr[1].grid()
+    axarr[1].set_yscale('log')
+    axarr[1].set_xlabel("Eb/N0")
     # axarr[1].set_xlim([-25,-3])
     axarr[1].set_ylabel("BER")
     axarr[1].set_title("Bit error rates in Rayleigh channel with AWGN")
