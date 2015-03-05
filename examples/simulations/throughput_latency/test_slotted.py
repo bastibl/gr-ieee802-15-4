e96042481fe42889db56aa8922be65fe0daa261f
Felix Wunsch <uncnr@student.kit.edu>
2015-03-05 14:55:52 +0100

    removed simulations folder

--- examples/simulations/throughput_latency/test_slotted.py
+++
@@ -1,9 +0,0 @@
-import mac_slotted as mac
-import parameters as params
-
-if __name__ == "__main__":
-    p = params.parameters("OQPSK", 0.001, 0.0)
-    m = mac.mac_slotted(p)
-    lat, bytes = m.run()
-    print "lat:", lat
-    print "bytes:", bytes
