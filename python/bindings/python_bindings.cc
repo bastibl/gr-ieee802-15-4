06ec8eb996db8354a44099cb69b16f44a1195daf
Bastian Bloessl <mail@bastibl.net>
2021-07-19 20:19:27 +0200

    python bindings

--- python/bindings/python_bindings.cc
+++ python/bindings/python_bindings.cc
@@ -21,6 +21,31 @@ namespace py = pybind11;
 // Please do not delete
 /**************************************/
 // BINDING_FUNCTION_PROTOTYPES(
+void bind_access_code_prefixer(py::module& m);
+void bind_access_code_removal_b(py::module& m);
+void bind_chips_to_bits_fb(py::module& m);
+void bind_codeword_demapper_ib(py::module& m);
+void bind_codeword_mapper_bi(py::module& m);
+void bind_codeword_soft_demapper_fb(py::module& m);
+void bind_deinterleaver_ff(py::module& m);
+void bind_dqcsk_demapper_cc(py::module& m);
+void bind_dqcsk_mapper_fc(py::module& m);
+void bind_dqpsk_mapper_ff(py::module& m);
+void bind_dqpsk_soft_demapper_cc(py::module& m);
+void bind_frame_buffer_cc(py::module& m);
+void bind_interleaver_ii(py::module& m);
+void bind_mac(py::module& m);
+void bind_multiuser_chirp_detector_cc(py::module& m);
+void bind_packet_sink(py::module& m);
+void bind_phr_prefixer(py::module& m);
+void bind_phr_removal(py::module& m);
+void bind_preamble_sfd_prefixer_ii(py::module& m);
+void bind_preamble_tagger_cc(py::module& m);
+void bind_qpsk_demapper_fi(py::module& m);
+void bind_qpsk_mapper_if(py::module& m);
+void bind_rime_stack(py::module& m);
+void bind_zeropadding_b(py::module& m);
+void bind_zeropadding_removal_b(py::module& m);
 // ) END BINDING_FUNCTION_PROTOTYPES
 
 
@@ -49,5 +74,30 @@ PYBIND11_MODULE(ieee802_15_4_python, m)
     // Please do not delete
     /**************************************/
     // BINDING_FUNCTION_CALLS(
+    bind_access_code_prefixer(m);
+    bind_access_code_removal_b(m);
+    bind_chips_to_bits_fb(m);
+    bind_codeword_demapper_ib(m);
+    bind_codeword_mapper_bi(m);
+    bind_codeword_soft_demapper_fb(m);
+    bind_deinterleaver_ff(m);
+    bind_dqcsk_demapper_cc(m);
+    bind_dqcsk_mapper_fc(m);
+    bind_dqpsk_mapper_ff(m);
+    bind_dqpsk_soft_demapper_cc(m);
+    bind_frame_buffer_cc(m);
+    bind_interleaver_ii(m);
+    bind_mac(m);
+    bind_multiuser_chirp_detector_cc(m);
+    bind_packet_sink(m);
+    bind_phr_prefixer(m);
+    bind_phr_removal(m);
+    bind_preamble_sfd_prefixer_ii(m);
+    bind_preamble_tagger_cc(m);
+    bind_qpsk_demapper_fi(m);
+    bind_qpsk_mapper_if(m);
+    bind_rime_stack(m);
+    bind_zeropadding_b(m);
+    bind_zeropadding_removal_b(m);
     // ) END BINDING_FUNCTION_CALLS
 }
